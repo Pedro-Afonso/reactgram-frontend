12855fbcd07c20606724da20bf856e5f83f1d246
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-15 12:16:24 -0300

    feature: Adicionado os temas Dark e Light

---
+++ src/shared/themes/Dark.ts
@@ -0,0 +1,29 @@
+import { createTheme } from "@mui/material";
+import { cyan, blueGrey } from "@mui/material/colors";
+
+export const DarkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: blueGrey[700],
+      dark: blueGrey[800],
+      light: blueGrey[500],
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: cyan[500],
+      dark: cyan[400],
+      light: cyan[300],
+      contrastText: "#ffffff",
+    },
+    background: {
+      default: "#202124",
+      paper: "#303134",
+    },
+  },
+  typography: {
+    allVariants: {
+      color: "white",
+    },
+  },
+});
---
+++ src/shared/themes/Light.ts
@@ -0,0 +1,23 @@
+import { createTheme } from "@mui/material";
+import { cyan, blueGrey } from "@mui/material/colors";
+
+export const LightTheme = createTheme({
+  palette: {
+    primary: {
+      main: blueGrey[700],
+      dark: blueGrey[800],
+      light: blueGrey[500],
+      contrastText: "#ffffff",
+    },
+    secondary: {
+      main: cyan[500],
+      dark: cyan[400],
+      light: cyan[300],
+      contrastText: "#ffffff",
+    },
+    background: {
+      default: "#f7f6f3",
+      paper: "#ffffff",
+    },
+  },
+});

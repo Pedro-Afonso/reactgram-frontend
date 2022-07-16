46e7290313a20b1d7d5c329d266943467d785304
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-16 14:33:49 -0300

    feature: Configuração do provedor do redux-toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import { BrowserRouter } from "react-router-dom";
 
 import { Navbar } from "./shared/components";
 import { AppRoutes } from "./routes";
-import { AppThemeProvider } from "./shared/contexts/ThemeContext";
+import { AppThemeProvider, AppReduxProvider } from "./shared/contexts";
 
 export const App = () => {
   return (
-    <AppThemeProvider>
-      <BrowserRouter>
-        <Navbar>
-          <AppRoutes />
-        </Navbar>
-      </BrowserRouter>
-    </AppThemeProvider>
+    <AppReduxProvider>
+      <AppThemeProvider>
+        <BrowserRouter>
+          <Navbar>
+            <AppRoutes />
+          </Navbar>
+        </BrowserRouter>
+      </AppThemeProvider>
+    </AppReduxProvider>
   );
 };

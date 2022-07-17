9ab255ca34deb9f4f5fb8499dba7c45ef8272577
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-17 11:44:27 -0300

    refactor: Alterado o nome do arquivo

--- src/shared/contexts/ReduxProvider.tsx
+++ src/shared/contexts/ReduxProvider.tsx
@@ -1,9 +1,5 @@
 import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-
-const store = configureStore({
-  reducer: {},
-});
+import { store } from "../../store";
 
 interface IAppReduxProviderProps {
   children: React.ReactNode;

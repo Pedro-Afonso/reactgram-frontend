26d923064091007be7f595439c9c6e8e52191727
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-17 12:02:17 -0300

    feature: Adicionado o slice de autenticação

---
+++ src/shared/slices/authSlice.ts
@@ -0,0 +1,68 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { IUser, IUserAuth, IUserState } from "../interface";
+import { authService } from "../services/authService";
+
+const localUser = localStorage.getItem("user");
+const user = localUser ? JSON.parse(localUser) : null;
+
+const initialState: IUserState = {
+  user: user ? user : null,
+  error: null,
+  success: false,
+  loading: false,
+};
+
+interface MyKnownError {
+  message: string;
+}
+
+// Register a user and sign in
+export const register = createAsyncThunk<
+  IUserAuth,
+  IUser,
+  {
+    rejectValue: MyKnownError;
+  }
+>("auth/register", async (user, thunkAPI) => {
+  const { user: userResponse, errors } = await authService.register(user);
+
+  // Check for errors
+  if (errors) {
+    return thunkAPI.rejectWithValue({ message: errors[0] });
+  }
+
+  return userResponse;
+});
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    reset: (state: IUserState) => {
+      state.loading = false;
+      state.error = null;
+      state.success = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload ? action.payload.message : "null";
+        state.user = null;
+      });
+  },
+});
+
+export const { reset } = authSlice.actions;
+export default authSlice.reducer;

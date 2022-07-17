e5d36802c2304505c80bc321b040c544a3e44c41
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-17 11:45:55 -0300

    feature: Adicionado a função register

---
+++ src/shared/services/authService.ts
@@ -0,0 +1,21 @@
+import { api, requestConfig, fecthRequest } from "../utils";
+
+import { IUser, IRegisterResponse } from "../interface";
+
+// Register a user
+const register = async (user: IUser) => {
+  const config = requestConfig("POST", user);
+
+  const res = await fecthRequest<IRegisterResponse>(
+    `${api}/users/register`,
+    config
+  );
+
+  if (res) {
+    localStorage.setItem("user", JSON.stringify(res));
+  }
+
+  return res;
+};
+
+export const authService = { register };

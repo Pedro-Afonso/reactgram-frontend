eae6eb10e9405a7cac65bdcfaa861a11d446193f
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-17 15:13:57 -0300

    feature: Adicionado o hook useAuth

---
+++ src/shared/hooks/useAuth.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import { useAppSelector } from "./useAppSelector";
+
+export const useAuth = () => {
+  const { user } = useAppSelector((state) => state.auth);
+
+  const [auth, setAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (user) {
+      setAuth(true);
+    } else {
+      setAuth(false);
+    }
+
+    setLoading(false);
+  }, [user]);
+
+  return { auth, loading };
+};

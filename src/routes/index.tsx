c01181272e0f1d5ec831fd0024fb87772ee807b7
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-17 15:45:21 -0300

    feat: Redireciona usuários com ou sem autenticação

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,39 @@
+import { Box, CircularProgress } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Home, Login, Register } from "../pages";
+import { useAuth } from "../shared/hooks";
 
 export const AppRoutes = () => {
+  const { auth, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <Box
+        width="100vw"
+        height="100vh"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <CircularProgress />;
+      </Box>
+    );
+  }
+
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route
+        path="/home"
+        element={auth ? <Home /> : <Navigate to="/login" />}
+      />
+      <Route
+        path="/login"
+        element={!auth ? <Login /> : <Navigate to="/home" />}
+      />
+      <Route
+        path="/register"
+        element={auth ? <Register /> : <Navigate to="/home" />}
+      />
 
       <Route path="*" element={<Navigate to="/home" />} />
     </Routes>

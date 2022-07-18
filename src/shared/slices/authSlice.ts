c6863e6b42346c2bbe38edd8e7ec5b7c8b673666
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-17 22:05:51 -0300

    feat: Adicionado o slice de login

--- src/shared/slices/authSlice.ts
+++ src/shared/slices/authSlice.ts
@@ -1,9 +1,9 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
 import { IUser, IUserAuth, IUserState } from "../interface";
 import { authService } from "../services/authService";
 
 const localUser = localStorage.getItem("user");
-const user = localUser ? JSON.parse(localUser) : null;
+const user: IUserAuth = localUser ? JSON.parse(localUser) : null;
 
 const initialState: IUserState = {
   user: user ? user : null,
@@ -17,27 +17,39 @@ interface MyKnownError {
 }
 
 // Register a user and sign in
-export const register = createAsyncThunk<
-  IUserAuth,
-  IUser,
-  {
-    rejectValue: MyKnownError;
-  }
->("auth/register", async (user, thunkAPI) => {
-  const { user: userResponse, errors } = await authService.register(user);
+export const register = createAsyncThunk(
+  "auth/register",
+  async (data: IUser, thunkAPI) => {
+    const res = await authService.register(data);
 
-  // Check for errors
-  if (errors) {
-    return thunkAPI.rejectWithValue({ message: errors[0] });
-  }
+    // Check for errors
+    if (res.errors) {
+      return thunkAPI.rejectWithValue(res.errors[0]);
+    }
 
-  return userResponse;
-});
+    return thunkAPI.fulfillWithValue(res);
+  }
+);
 
 // Logout a user
 export const logout = createAsyncThunk("auth/logout", () => {
   authService.logout();
 });
+
+// Sign in a user
+export const login = createAsyncThunk(
+  "auth/login",
+  async (user: Omit<IUser, "name" | "confirmPassword">, thunkAPI) => {
+    const res = await authService.login(user);
+
+    // Check for errors
+    if (res.errors) {
+      return thunkAPI.rejectWithValue(res.errors[0]);
+    }
+
+    return thunkAPI.fulfillWithValue(res);
+  }
+);
 
 export const authSlice = createSlice({
   name: "auth",
@@ -47,31 +59,58 @@ export const authSlice = createSlice({
       state.loading = false;
       state.error = null;
       state.success = false;
+      state.user = null;
     },
   },
-  extraReducers: (builder) => {
-    builder
-      .addCase(register.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(register.fulfilled, (state, action) => {
+  extraReducers(builder) {
+    builder.addCase(register.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(
+      register.fulfilled.type,
+      (state, action: PayloadAction<IUserAuth>) => {
         state.loading = false;
         state.success = true;
         state.error = null;
         state.user = action.payload;
-      })
-      .addCase(register.rejected, (state, action) => {
+      }
+    );
+    builder.addCase(
+      register.rejected.type,
+      (state, action: PayloadAction<string>) => {
         state.loading = false;
-        state.error = action.payload ? action.payload.message : null;
+        state.error = action.payload ? action.payload : null;
         state.user = null;
-      })
-      .addCase(logout.fulfilled, (state) => {
+      }
+    );
+    builder.addCase(logout.fulfilled, (state) => {
+      state.loading = false;
+      state.success = true;
+      state.error = null;
+      state.user = null;
+    });
+    builder.addCase(login.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(
+      login.fulfilled.type,
+      (state, action: PayloadAction<IUserAuth>) => {
         state.loading = false;
         state.success = true;
         state.error = null;
+        state.user = action.payload;
+      }
+    );
+    builder.addCase(
+      login.rejected.type,
+      (state, action: PayloadAction<string>) => {
+        state.loading = false;
+        state.error = action.payload ? action.payload : null;
         state.user = null;
-      });
+      }
+    );
   },
 });
 

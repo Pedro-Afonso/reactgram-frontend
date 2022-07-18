c7b884f0750400e0190584eb5e96665bc0582f68
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-18 19:37:38 -0300

    feat: Adicionado CssBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter } from "react-router-dom";
 import { AppThemeProvider, AppReduxProvider } from "./shared/contexts";
 import { Navbar } from "./shared/components";
 import { AppRoutes } from "./routes";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export const App = () => {
   return (
     <AppReduxProvider>
       <AppThemeProvider>
+        <CssBaseline />
         <BrowserRouter>
           <Navbar>
             <AppRoutes />

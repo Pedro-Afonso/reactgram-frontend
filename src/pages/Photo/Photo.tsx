002e46e7e922efbd0665ade5348f78886f9aee50
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-23 14:24:25 -0300

    feat: Adicionado botão de like

--- src/pages/Photo/Photo.tsx
+++ src/pages/Photo/Photo.tsx
@@ -2,19 +2,27 @@ import { Paper } from "@mui/material";
 import { Box } from "@mui/system";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { LikeButton } from "../../shared/components";
 import { PhotoItem } from "../../shared/components/PhotoItem/PhotoItem";
 import { useAppDispatch, useAppSelector } from "../../shared/hooks";
-import { getPhoto } from "../../shared/slices";
+import { IPhoto, IUserIdToken } from "../../shared/interface";
+import { getPhoto, likePhoto } from "../../shared/slices";
 
 export const Photo = () => {
   const { id } = useParams();
 
   const dispatch = useAppDispatch();
 
-  const { user } = useAppSelector((state) => state.auth);
+  const { user } = useAppSelector((state) => state.auth) as {
+    user: IUserIdToken;
+  };
   const { photo, loading, error, message } = useAppSelector(
     (state) => state.photo
   );
+
+  const handleLike = (photoId: string) => {
+    dispatch(likePhoto(photoId));
+  };
 
   // Load photo data
   useEffect(() => {
@@ -37,7 +45,12 @@ export const Photo = () => {
       padding={2}
       component={Paper}
     >
-      {photo && <PhotoItem photo={photo} />}
+      {photo && (
+        <>
+          <PhotoItem photo={photo} />
+          <LikeButton photo={photo} user={user} handleLike={handleLike} />)
+        </>
+      )}
     </Box>
   );
 };
---
+++ src/shared/components/LikeButton/LikeButton.tsx
@@ -0,0 +1,41 @@
+import { Box, Icon, IconButton, Paper, Typography } from "@mui/material";
+import { IPhoto, IUserIdToken } from "../../interface";
+
+interface ILikeProps {
+  photo: IPhoto;
+  user: IUserIdToken;
+  handleLike: (photoId: string) => void;
+}
+
+export const LikeButton: React.FC<ILikeProps> = ({
+  photo,
+  user,
+  handleLike,
+}) => {
+  return (
+    <Box
+      component={Paper}
+      width="100%"
+      padding={2}
+      display="flex"
+      justifyContent="end"
+      alignItems="center"
+      variant="outlined"
+    >
+      {photo.likes && user && (
+        <>
+          {photo.likes.includes(user._id) ? (
+            <IconButton>
+              <Icon>favorite</Icon>
+            </IconButton>
+          ) : (
+            <IconButton onClick={() => handleLike(photo._id)}>
+              <Icon>favorite_border</Icon>
+            </IconButton>
+          )}
+          <Typography>{photo.likes.length} like(s)</Typography>
+        </>
+      )}
+    </Box>
+  );
+};

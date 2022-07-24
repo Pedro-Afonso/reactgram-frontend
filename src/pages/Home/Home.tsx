3c64bc473b20ae05569dd5d57811d443bb52876b
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-24 13:48:17 -0300

    feat: Adicionado a visualização de todas as fotos

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,52 @@
+import { Box, Paper } from "@mui/material";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { LikeButton, PhotoItem } from "../../shared/components";
+import { useAppDispatch, useAppSelector } from "../../shared/hooks";
+import { IUserIdToken } from "../../shared/interface";
+import { getAllPhotos, likePhoto } from "../../shared/slices";
+import { api } from "../../shared/utils";
+
 export const Home = () => {
-  return <h1>Home</h1>;
+  const dispatch = useAppDispatch();
+
+  const navigate = useNavigate();
+
+  const { photos, loading } = useAppSelector((state) => state.photo);
+
+  const { user: authUser } = useAppSelector((state) => state.auth) as {
+    user: IUserIdToken;
+  };
+
+  // Load photos
+  useEffect(() => {
+    dispatch(getAllPhotos());
+  }, []);
+
+  const handleLike = (photoId: string) => {
+    dispatch(likePhoto(photoId));
+  };
+
+  return (
+    <Box
+      maxWidth={700}
+      marginX="auto"
+      marginTop={10}
+      paddingY={2}
+      //component={Paper}
+    >
+      {photos &&
+        photos.map((photo, key) => (
+          <Box key={key} marginBottom={2}>
+            <PhotoItem photo={photo} linkControl />
+
+            <LikeButton
+              user={authUser}
+              photo={photo}
+              handleLike={() => handleLike(photo._id)}
+            />
+          </Box>
+        ))}
+    </Box>
+  );
 };

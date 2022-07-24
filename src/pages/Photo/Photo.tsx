a8eab742197d6efc59a17d5f966c98a445b2368b
Pedro-Afonso <pedro.766@hotmail.com>
2022-07-24 13:48:42 -0300

    feat: Adicionado a funcionalidade de comentar

--- src/pages/Photo/Photo.tsx
+++ src/pages/Photo/Photo.tsx
@@ -1,12 +1,14 @@
-import { Paper } from "@mui/material";
+import { LoadingButton } from "@mui/lab";
+import { Avatar, Paper, TextField, Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { LikeButton } from "../../shared/components";
 import { PhotoItem } from "../../shared/components/PhotoItem/PhotoItem";
 import { useAppDispatch, useAppSelector } from "../../shared/hooks";
-import { IPhoto, IUserIdToken } from "../../shared/interface";
-import { getPhoto, likePhoto } from "../../shared/slices";
+import { IUserIdToken } from "../../shared/interface";
+import { commentPhoto, getPhoto, likePhoto } from "../../shared/slices";
+import { uploads } from "../../shared/utils";
 
 export const Photo = () => {
   const { id } = useParams();
@@ -19,6 +21,8 @@ export const Photo = () => {
   const { photo, loading, error, message } = useAppSelector(
     (state) => state.photo
   );
+
+  const [textComment, setTextComment] = useState("");
 
   const handleLike = (photoId: string) => {
     dispatch(likePhoto(photoId));
@@ -33,6 +37,23 @@ export const Photo = () => {
     console.log(photo);
   }, [id]);
 
+  const handleComment = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!id) {
+      return;
+    }
+
+    const commentData = {
+      comment: textComment,
+      id: id,
+    };
+
+    dispatch(commentPhoto(commentData));
+
+    setTextComment("");
+  };
+
   if (loading) {
     return <p>Carregando...</p>;
   }
@@ -42,13 +63,47 @@ export const Photo = () => {
       maxWidth={700}
       marginX="auto"
       marginTop={10}
-      padding={2}
+      paddingY={2}
       component={Paper}
     >
       {photo && (
         <>
           <PhotoItem photo={photo} />
-          <LikeButton photo={photo} user={user} handleLike={handleLike} />)
+          <LikeButton photo={photo} user={user} handleLike={handleLike} />
+          <form onSubmit={handleComment}>
+            <Box width="100%" padding={2}>
+              <Box>
+                <TextField
+                  fullWidth
+                  variant="standard"
+                  label="Deixe o seu comentário:"
+                  type="text"
+                  value={textComment}
+                  onChange={(e) => setTextComment(e.target.value)}
+                />
+              </Box>
+              <Box marginTop={4}>
+                <LoadingButton type="submit" fullWidth variant="contained">
+                  Enviar
+                </LoadingButton>
+              </Box>
+            </Box>
+          </form>
+          <Box>
+            {photo.comments.map((comment, key) => (
+              <Box width="100%" padding={2} key={key}>
+                <Typography>{comment.comment}</Typography>
+                <Box>
+                  <Typography>{comment.userName}</Typography>
+                  <Avatar
+                    sx={{ width: 32, height: 32 }}
+                    src={`${uploads}/users/${comment.userImage}`}
+                    alt={comment.userName}
+                  />
+                </Box>
+              </Box>
+            ))}
+          </Box>
         </>
       )}
     </Box>

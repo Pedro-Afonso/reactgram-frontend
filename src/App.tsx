cd6ca7aa724c988ec6564e29e172589ef6e55c0f
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-03 19:55:54 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { AppRoutes } from './routes'
 import CssBaseline from '@mui/material/CssBaseline'
 
 export const App = () => {
+  console.log(import.meta.env.VITE_API)
   return (
     <AppReduxProvider>
       <AppThemeProvider>

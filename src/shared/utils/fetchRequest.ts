c8250bd52d811a7677a22d203e0376e1b23deaad
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-04 11:30:18 -0300

    update

--- src/shared/utils/fetchRequest.ts
+++ src/shared/utils/fetchRequest.ts
@@ -2,6 +2,7 @@ export const fecthRequest = async <TResponse>(
   url: string,
   config: RequestInit = {}
 ): Promise<TResponse> => {
+  console.warn(url)
   const response = await fetch(url, config)
   const data = await response.json()
   return data as TResponse

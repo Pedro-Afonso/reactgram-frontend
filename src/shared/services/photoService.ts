f8cb68c18c372382672ea316f0dffaebb9a73976
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-11 13:25:11 -0300

    feat: Remove commentPhoto service

--- src/shared/services/photoService.ts
+++ src/shared/services/photoService.ts
@@ -1,140 +1,86 @@
-import { IPhotoResponse } from '../interface'
+import {
+  TDeleteRes,
+  TPhotoRes,
+  TUpdatePhotoRes,
+  TLikePhotoRes,
+  TPhotosRes
+} from '../interface'
 import { api, fecthRequest, requestConfig } from '../utils'
-import { handleError } from '../utils/handleError'
+import { tryCatchService } from '../utils/tryCatchService'
 
 // Publish a photo
 const publishPhoto = async (data: FormData, token: string) => {
   const config = requestConfig('POST', data, token, true)
 
-  let res: IPhotoResponse = {}
-
-  try {
-    res = await fecthRequest<IPhotoResponse>(`${api}/photos`, config)
-  } catch (error) {
-    res.errors = [handleError(error)]
-  }
-  return res
+  return tryCatchService(async () => {
+    return await fecthRequest<TPhotoRes>(`${api}/photos`, config)
+  })
 }
 
 // Get all user photos
 const getUserPhotos = async (id: string, token: string) => {
   const config = requestConfig('GET', null, token)
-  let res: IPhotoResponse = {}
 
-  try {
-    res = await fecthRequest<IPhotoResponse>(`${api}/photos/user/${id}`, config)
-  } catch (error) {
-    res.errors = [handleError(error)]
-  }
-  return res
+  return tryCatchService(async () => {
+    return await fecthRequest<TPhotosRes>(`${api}/photos/user/${id}`, config)
+  })
 }
 
 // Delete a photo
 const deletePhoto = async (id: string, token: string) => {
   const config = requestConfig('DELETE', null, token)
 
-  let res: IPhotoResponse = {}
-
-  try {
-    res = await fecthRequest<IPhotoResponse>(`${api}/photos/${id}`, config)
-  } catch (error) {
-    res.errors = [handleError(error)]
-  }
-  return res
+  return tryCatchService(async () => {
+    return fecthRequest<TDeleteRes>(`${api}/photos/${id}`, config)
+  })
 }
 
 // Update a user photos
 const updatePhoto = async (title: string, id: string, token: string) => {
   const config = requestConfig('PUT', { title }, token)
 
-  let res: IPhotoResponse = {}
-
-  try {
-    res = await fecthRequest<IPhotoResponse>(`${api}/photos/${id}`, config)
-  } catch (error) {
-    res.errors = [handleError(error)]
-  }
-  return res
+  return tryCatchService(async () => {
+    return await fecthRequest<TUpdatePhotoRes>(`${api}/photos/${id}`, config)
+  })
 }
 
 // Get user photo by id
 const getPhoto = async (id: string) => {
   const config = requestConfig('GET')
 
-  let res: IPhotoResponse = {}
-
-  try {
-    res = await fecthRequest<IPhotoResponse>(`${api}/photos/${id}`, config)
-  } catch (error) {
-    res.errors = [handleError(error)]
-  }
-  return res
+  return tryCatchService(async () => {
+    return await fecthRequest<TPhotoRes>(`${api}/photos/${id}`, config)
+  })
 }
 
 // Like a photo
 const likePhoto = async (id: string, token: string) => {
   const config = requestConfig('PUT', null, token)
 
-  let res: IPhotoResponse = {}
-
-  try {
-    res = await fecthRequest<IPhotoResponse>(`${api}/photos/like/${id}`, config)
-  } catch (error) {
-    res.errors = [handleError(error)]
-  }
-  return res
-}
-
-// Add comment to a photo
-const commentPhoto = async (
-  id: string,
-  data: { comment: string },
-  token: string
-) => {
-  const config = requestConfig('PUT', data, token)
-
-  let res: IPhotoResponse = {}
-
-  try {
-    res = await fecthRequest<IPhotoResponse>(
-      `${api}/photos/comment/${id}`,
-      config
-    )
-  } catch (error) {
-    res.errors = [handleError(error)]
-  }
-  return res
+  return tryCatchService(async () => {
+    return await fecthRequest<TLikePhotoRes>(`${api}/photos/like/${id}`, config)
+  })
 }
 
 // Get all photos
 const getAllPhotos = async (token: string) => {
   const config = requestConfig('GET', null, token)
 
-  let res: IPhotoResponse = {}
-
-  try {
-    res = await fecthRequest<IPhotoResponse>(`${api}/photos`, config)
-  } catch (error) {
-    res.errors = [handleError(error)]
-  }
-  return res
+  return tryCatchService(async () => {
+    return await fecthRequest<TPhotosRes>(`${api}/photos`, config)
+  })
 }
 
 // Search photo by title
 const searchPhotos = async (query: string, token: string) => {
   const config = requestConfig('GET', null, token)
 
-  let res: IPhotoResponse = {}
-
-  try {
-    res = await fecthRequest<IPhotoResponse>(
+  return tryCatchService(async () => {
+    return await fecthRequest<TPhotosRes>(
       `${api}/photos/search?q=${query}`,
       config
     )
-  } catch (error) {
-    res.errors = [handleError(error)]
-  }
-  return res
+  })
 }
 
 export const photoService = {
@@ -144,7 +90,6 @@ export const photoService = {
   updatePhoto,
   getPhoto,
   likePhoto,
-  commentPhoto,
   getAllPhotos,
   searchPhotos
 }

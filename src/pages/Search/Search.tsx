0087bc42d20d13644de260d13e7a1e82eb30de6e
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-11 20:48:31 -0300

    feat: Show text for seach results

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,6 +1,6 @@
+import { Box, Backdrop, CircularProgress, Typography } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
-import { Box, Backdrop, CircularProgress } from '@mui/material'
 
 import { likePhoto, searchPhotos } from '../../shared/slices/photoSlice'
 import { useAppDispatch, useAppSelector } from '../../shared/hooks'
@@ -38,6 +38,7 @@ export const Search = () => {
 
   return (
     <Box maxWidth={700} marginX="auto" marginTop={10} paddingY={2}>
+      <Typography>{`Mostrando buscas para "${search}"`}</Typography>
       {photos &&
         photos.map((photo, key) => (
           <Box key={key} marginBottom={2}>

6ef6a6cf14b193ee3c616496eb9bdb5c3cf755ec
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-26 16:58:54 -0300

    feat: Add GalleryItem component

---
+++ src/shared/components/GalleryItem/index.tsx
@@ -0,0 +1,61 @@
+import { useNavigate, useParams } from 'react-router-dom'
+
+import ImageListItemBar from '@mui/material/ImageListItemBar'
+import ImageListItem from '@mui/material/ImageListItem'
+import IconButton from '@mui/material/IconButton'
+import Icon from '@mui/material/Icon'
+
+import { useAppDispatch, useAppSelector } from '../../hooks'
+import { deletePhoto } from '../../slices/photoSlice'
+import { DialogEditPhoto } from '../../components'
+import { TPhoto } from '../../interface'
+
+interface IGalleryItemProps {
+  photo: TPhoto
+}
+
+export const GalleryItem: React.FC<IGalleryItemProps> = ({ photo }) => {
+  const { id } = useParams()
+
+  const dispatch = useAppDispatch()
+
+  const userAuth = useAppSelector(state => state.auth.user)
+
+  const navigate = useNavigate()
+
+  const isTheProfileOwner = id === userAuth!._id
+
+  // Delete a photo
+  const handleDelete = () => {
+    dispatch(deletePhoto(photo._id))
+  }
+
+  return (
+    <ImageListItem key={photo._id}>
+      {photo.image && <img src={photo.image} alt={photo.title} />}
+
+      <ImageListItemBar
+        actionIcon={
+          <>
+            <IconButton onClick={() => navigate(`/photos/${photo._id}`)}>
+              <Icon>visibility</Icon>
+            </IconButton>
+            {isTheProfileOwner && (
+              <>
+                {/* Edit Button */}
+                <DialogEditPhoto photo={photo} />
+                {/* /Edit Button */}
+
+                {/* Delete Button */}
+                <IconButton onClick={handleDelete}>
+                  <Icon>delete</Icon>
+                </IconButton>
+                {/* /Delete Button */}
+              </>
+            )}
+          </>
+        }
+      />
+    </ImageListItem>
+  )
+}

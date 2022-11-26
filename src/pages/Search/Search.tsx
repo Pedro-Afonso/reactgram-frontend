3b967c2778df01462a869698b9cd1355ee68a5f7
Pedro-Afonso <pedro.766@hotmail.com>
2022-11-26 17:21:08 -0300

    feat: Replace components with PhotoFeed component

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,21 +1,17 @@
 import { Box, Backdrop, CircularProgress, Typography } from '@mui/material'
-import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
 
-import { likePhoto, searchPhotos } from '../../shared/slices/photoSlice'
 import { useAppDispatch, useAppSelector } from '../../shared/hooks'
-import { addComment } from '../../shared/slices/commentSlice'
+import { searchPhotos } from '../../shared/slices/photoSlice'
 import { useQuery } from '../../shared/hooks/useQuery'
-import { PhotoItem } from '../../shared/components'
+import { PhotoFeed } from '../../shared/components'
 
 export const Search = () => {
   const dispatch = useAppDispatch()
 
-  const { user: authUser, loading: loadingAuth } = useAppSelector(
-    state => state.auth
-  )
+  const loadingAuth = useAppSelector(state => state.auth.loading)
 
-  const { photos, loading: loadingPhoto } = useAppSelector(state => state.photo)
+  const loadingPhoto = useAppSelector(state => state.photo.loading)
 
   const query = useQuery()
   const search = query.get('q')
@@ -26,31 +22,10 @@ export const Search = () => {
     }
   }, [search, dispatch])
 
-  const navigate = useNavigate()
-
-  const handleLike = (photoId: string) => {
-    dispatch(likePhoto(photoId))
-  }
-
-  const handleSubmitComment = (comment: string, photoId: string) => {
-    dispatch(addComment({ comment, photoId }))
-  }
-
   return (
     <Box maxWidth={700} marginX="auto" marginTop={10} paddingY={2}>
       <Typography>{`Mostrando buscas para "${search}"`}</Typography>
-      {photos &&
-        photos.map((photo, key) => (
-          <Box key={key} marginBottom={2}>
-            <PhotoItem
-              authUser={authUser}
-              photo={photo}
-              handleLike={handleLike}
-              navigate={navigate}
-              handleSubmitComment={handleSubmitComment}
-            />
-          </Box>
-        ))}
+      <PhotoFeed />
       <Backdrop
         sx={{ color: '#fff', zIndex: theme => theme.zIndex.modal + 1 }}
         open={loadingAuth || loadingPhoto}
